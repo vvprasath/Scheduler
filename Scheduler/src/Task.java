2ecbf7b6dd9746810e11c35d5ed6525efed01b59
vvprasath <vvprasath@gmail.com>
2016-11-14 17:02:24 +0530

    Data container

---
+++ Scheduler/src/Task.java
@@ -0,0 +1,22 @@
+
+public class Task {
+	int taskid;
+	String task;
+	int duration;
+	
+	public int gettaskid()
+	{
+		return taskid;
+	}
+	
+	public String gettask()
+	{
+		return task;
+	}
+	
+	public int getduration()
+	{
+		return duration;
+	}
+
+}
